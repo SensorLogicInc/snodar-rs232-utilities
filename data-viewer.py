7ff95d2879cf24b9fb67a67049a5cc238f39fb73
Trevor Vannoy <trevor@vannoy.fyi>
2025-05-23 16:04:04 -0600

    get serial data from a separate thread to make real-time plotting easier

--- data-viewer.py
+++ data-viewer.py
@@ -1,36 +1,10 @@
 import serial
 import pandas as pd
+from queue import Queue
+import threading
 
-if __name__ == "__main__":
-
-    df = pd.DataFrame(
-        columns=[
-            "Time",
-            "Current (mA)",
-            "Voltage (V)",
-            "NRF Temperature",
-            "PCB Temperature",
-            "IMU Temperature",
-            "IMU Roll",
-            "IMU Pitch",
-            "IMU Yaw",
-            "IMU Flag",
-            "Lidar SoC Temperature",
-            "Lidar PCB Temperature",
-            "Lidar Distance",
-            "Heater Enabled",
-            "Outside Temperature",
-            "Seasonsal Snow Depth",
-            "Seasonal Snow Fall",
-            "New Snow Fall",
-            "DoY SWE",
-            "Temp SWE",
-        ],
-    )
-
+def read_rs232_data(queue):
     serial_port = serial.Serial(port="/dev/ttyUSB0", baudrate=19200)
-
-    print(df.columns)
 
     while True:
         raw_bytes = serial_port.readline()
@@ -41,9 +15,45 @@ if __name__ == "__main__":
 
         data = [float(x) for x in csv_list]
 
-        df.loc[len(df)] = data
+        queue.put(data)
 
-        # print(df)
-        print(data)
-
+    # TODO: graceful exit
     serial_port.close()
+
+
+if __name__ == "__main__":
+
+# df = pd.DataFrame(
+#     columns=[
+#         "Time",
+#         "Current (mA)",
+#         "Voltage (V)",
+#         "NRF Temperature",
+#         "PCB Temperature",
+#         "IMU Temperature",
+#         "IMU Roll",
+#         "IMU Pitch",
+#         "IMU Yaw",
+#         "IMU Flag",
+#         "Lidar SoC Temperature",
+#         "Lidar PCB Temperature",
+#         "Lidar Distance",
+#         "Heater Enabled",
+#         "Outside Temperature",
+#         "Seasonal Snow Depth",
+#         "Seasonal Snow Fall",
+#         "New Snow Fall",
+#         "DoY SWE",
+#         "Temp SWE",
+#     ],
+# )
+
+    queue = Queue()
+
+    rs232_thread = threading.Thread(target=read_rs232_data, args=(queue,))
+    rs232_thread.start()
+
+    while True:
+        if not queue.empty():
+            print(queue.get())
+
